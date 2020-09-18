ddad237cf3ce99321ccc2abe41c9aa8d61d04adf
Mengxi Zhang <zmx94824@gmail.com>
2020-09-18 06:19:26 -0700

    Split into two endpoints: game start and status

--- src/main/java/mengxi/blackjack_server/BlackjackServerApplication.java
+++ src/main/java/mengxi/blackjack_server/BlackjackServerApplication.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import mengxi.blackjack_server.db.service.PlayerService;
 import mengxi.blackjack_server.game.Game;
 import mengxi.blackjack_server.game.GameImpl;
-import mengxi.blackjack_server.http_msg.GameStartResponse;
 import mengxi.blackjack_server.db.entity.Player;
 
 @SpringBootApplication
@@ -22,6 +22,7 @@ import mengxi.blackjack_server.db.entity.Player;
 public class BlackjackServerApplication {
 
 	private PlayerService playerService;
+	private Game g;
 
 	@Autowired
 	public void setPlayerService(PlayerService playerService) {
@@ -43,14 +44,24 @@ public class BlackjackServerApplication {
 		return playerService.getAll();
 	}
 
-	@PostMapping("/start")
-	public GameStartResponse start() {
-		Game g = new GameImpl();
-		g.start(UUID.randomUUID());
+	@GetMapping("/game/status")
+	public List<List<String>> status() {
+		return new ArrayList<>() {
+			private static final long serialVersionUID = 1L;
+			{
+				if (g != null) {
+					this.add(g.getPlayerCards());
+					this.add(g.getDealerCards());
+				}
+			}
+		};
+	}
 
-		return new GameStartResponse(
-			g.getGameId(), g.getPlayerCards(), g.getDealerCards()
-		);
+	@PostMapping("/game/start")
+	public UUID start(@RequestParam UUID playerId) {
+		g = new GameImpl();
+		g.start(playerId);
+        return g.getGameId();
 	}
 
 	public static void main(String[] args) {
