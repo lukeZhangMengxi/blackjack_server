ef0579a61453ca315f495a5356788752b38add38
Mengxi Zhang <zmx94824@gmail.com>
2020-10-22 19:43:06 -0700

    Add unit tests for creating user and authentication

--- src/test/java/mengxi/blackjack_server/db/PlayerServiceTest.java
+++ src/test/java/mengxi/blackjack_server/db/PlayerServiceTest.java
@@ -1,19 +1,25 @@
 package mengxi.blackjack_server.db;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import mengxi.blackjack_server.db.dao.PlayerDAO;
 import mengxi.blackjack_server.db.dao.PlayerDAOImpl;
 import mengxi.blackjack_server.db.entity.Player;
+import mengxi.blackjack_server.db.entity.PlayerWithCredentials;
 import mengxi.blackjack_server.db.service.PlayerService;
 import mengxi.blackjack_server.db.service.PlayerServiceImpl;
 import mengxi.blackjack_server.TestUtils;
@@ -68,6 +74,35 @@ public class PlayerServiceTest {
         when(mockDAO.getBalance(sampleId)).thenReturn(1000l);
 
         test.updateBalance(sampleId, -1500);
+    }
+
+    @Test
+    public void createUserTest() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        ArgumentCaptor<String> passwordHashCaptor = ArgumentCaptor.forClass(String.class);
+        when(mockDAO.createPlayer(anyString(), anyString(), passwordHashCaptor.capture(), anyString()))
+                .thenReturn(sampleId);
+
+        assertEquals(sampleId, test.createUser("email", "displayName", "passwordRaw"));
+        assertNotEquals("passwordRaw", passwordHashCaptor.getValue()); // Password should be hashed
+    }
+
+    @Test
+    public void authenticateTest() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        // Assuming SHA-512 hashing algorithm
+        String passwordRawStr = "MyPassword";
+        String saltStr = "Éj,µêß7ã1ß+ÒïÏörh";
+        String passwordHashStr = "79bd530c9366c5c2195d64ca4fad6cd11767eeca7fbe9243483e0eada75539817f5f09c48b36743e8af26280f4f8bcdeead43d6b6d0264c216e51156983df818";
+
+        when(mockDAO.getPlayer("email", PlayerWithCredentials.class)).thenReturn(new PlayerWithCredentials() {
+            {
+                this.setId(sampleId);
+                this.setEmail("email");
+                this.setSalt(saltStr);
+                this.setPasswordHash(passwordHashStr);
+            }
+        });
+
+        assertEquals(sampleId, test.authenticate("email", passwordRawStr));
     }
 
 }
