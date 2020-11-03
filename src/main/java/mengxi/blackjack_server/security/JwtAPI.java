e844ab8c3fd44a13f5db8d94bd9a6fb2236ea9d3
Mengxi Zhang <zmx94824@gmail.com>
2020-11-02 21:57:57 -0800

    jwt check by email or playerId

--- src/main/java/mengxi/blackjack_server/security/JwtAPI.java
+++ src/main/java/mengxi/blackjack_server/security/JwtAPI.java
@@ -9,20 +9,26 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JwtAPI {
+
+    public enum ClaimType {
+        EMAIL, PLAYERID
+    }
+
     private static String ISSUER = "Mengxi";
     private static String SECRET = "random-string";
     private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET);
     private static JWTVerifier verifier = JWT.require(ALGORITHM).withIssuer(ISSUER).acceptLeeway(1).build();
 
-    public static String generateToken(String email, int minutes) {
-        return JWT.create().withIssuer(ISSUER).withClaim("email", email)
+    public static String generateToken(String email, String playerId, int minutes) {
+        return JWT.create().withIssuer(ISSUER).withClaim(ClaimType.EMAIL.toString(), email)
+                .withClaim(ClaimType.PLAYERID.toString(), playerId)
                 .withExpiresAt(new Date(System.currentTimeMillis() + minutes * 60 * 1000)).sign(ALGORITHM);
     }
 
-    public static boolean verifyToken(String token, String email) {
+    public static boolean verifyToken(String token, String email, ClaimType type) {
         try {
             DecodedJWT jwt = verifier.verify(token);
-            return jwt.getClaim("email").asString().equals(email);
+            return jwt.getClaim(type.toString()).asString().equals(email);
         } catch (TokenExpiredException e) {
             // Log it
             return false;
